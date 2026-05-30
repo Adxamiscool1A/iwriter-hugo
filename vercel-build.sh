10da69424e6b1eb6e4a1125ef14c71f62f8dddd3
Abryce4 <Abryce4@proton.me>
2026-05-30 16:43:29 +1000

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -4,26 +4,18 @@ set -e
 GO_VERSION='1.22.5'
 HUGO_VERSION='0.148.2'
 
-echo "USING NODE VERSION: $(node -v)"
-
-echo "Installing Go $GO_VERSION..."
+echo "Installing Go..."
 curl -L -o go.tar.gz https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
 tar -xzf go.tar.gz
 export PATH="$PWD/go/bin:$PATH"
 go version
 
-echo "Installing Hugo $HUGO_VERSION..."
+echo "Installing Hugo..."
 curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
 tar -xzf hugo.tar.gz
 chmod +x hugo
 export PATH="$PWD:$PATH"
 hugo version
 
-echo "Project setting up..."
-npm run project-setup
-
-echo "Installing dependencies..."
-npm install
-
-echo "Building site..."
-npm run build
+echo "Building Hugo site..."
+hugo --themesDir ../.. --gc --minify
