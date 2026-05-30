30ae165fc0e7011a4d5b2532cbabc631033327fe
Abryce4 <Abryce4@proton.me>
2026-05-30 16:41:23 +1000

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
+set -e
 
-NODE_VERSION='24.9.0'
-GO_VERSION='1.24.4'
+GO_VERSION='1.22.5'
 HUGO_VERSION='0.148.2'
 
 echo "USING NODE VERSION: $(node -v)"
 
 echo "Installing Go $GO_VERSION..."
-curl -sSOL https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
-tar -xzf go${GO_VERSION}.linux-amd64.tar.gz
-export PATH=$PATH:$PWD/go/bin
-rm -rf go${GO_VERSION}.linux-amd64.tar.gz
+curl -L -o go.tar.gz https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+tar -xzf go.tar.gz
+export PATH="$PWD/go/bin:$PATH"
 go version
 
 echo "Installing Hugo $HUGO_VERSION..."
-curl -sSOL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
-tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+tar -xzf hugo.tar.gz
 chmod +x hugo
-export PATH=$PATH:$PWD
-rm -rf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+export PATH="$PWD:$PATH"
 hugo version
 
 echo "Project setting up..."
 npm run project-setup
 
-echo "Installing project dependencies..."
+echo "Installing dependencies..."
 npm install
 
-echo "Running the build command..."
+echo "Building site..."
 npm run build
